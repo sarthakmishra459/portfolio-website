7b34ba620d81417eb70eec709ed3cd393d22dc90
Sarthak Mishra <124512392+sarthakmishra459@users.noreply.github.com>
2023-09-13 17:30:08 +0530

    Portfolio Website

--- pages/services/index.js
+++ pages/services/index.js
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 import { fadeIn } from '../../variants';
 const Services = () => {
   return (
-    <div className='bg-primary/30 py-16 flex items-center'>
+    <div className='h-full bg-primary/30  flex items-center'>
       <Circles />
       <div className='container mx-auto'>
         <div className='flex flex-col xl:flex-row gap-x-8'>
--- pages/work/index.js
+++ pages/work/index.js
@@ -6,7 +6,7 @@ import { fadeIn } from '../../variants';
 
 const Work = () => {
   return (
-    <div className='h-30 bg-primary/30 py-19 flex items-center'>
+    <div className='h-full bg-primary/30 py-36 flex items-center'>
       <Circles />
       <div className='container mx-auto'>
         <div className='flex flex-col xl:flex-row gap-x-8'>
